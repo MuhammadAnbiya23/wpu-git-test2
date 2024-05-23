fa76f1b62e4ce1edfe4cca1950342bf2a7b02d65
MuhammadAnbiya23 <anbiya17agustus@gmail.com>
2024-05-23 07:20:16 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
 </head>
 <body>
     <h1>Hello World</h1>
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint laudantium cumque, ullam quia saepe eius iusto inventore quibusdam facilis obcaecati! Voluptatum porro consectetur nobis nesciunt, magni possimus quod consequatur veritatis!    
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint laudantium cumque, ullam quia saepe eius iusto inventore quibusdam facilis obcaecati! Voluptatum porro consectetur nobis nesciunt, magni possimus quod consequatur veritatis!
+    <li>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla dolor ab necessitatibus at dolorem sint placeat corrupti nostrum possimus, quas quidem. Soluta ducimus quam earum architecto possimus eos natus atque?
+    </li>
 </body>
 </html>
